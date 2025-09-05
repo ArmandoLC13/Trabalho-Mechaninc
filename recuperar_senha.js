551500e7c29ff7df6a38c067e757da50d5a82904
ArmandoLC13 <armandoschoenstatt@gmail.com>
2025-09-05 20:33:42 -0300

    Delete recuperar_senha.js

--- recuperar_senha.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('forgot-password-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('email-recuperacao').value;
-    alert(`Um link de recuperação de senha foi enviado para ${email}.`);
-    // Aqui você adicionaria a lógica real para enviar o e-mail de recuperação
-});
-
